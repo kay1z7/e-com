18f27532a4157124d4e09d97e5506f468f65324d
kay1z7 <103060251+kay1z7@users.noreply.github.com>
2025-04-28 18:18:55 +0600

    Delete custom.d.ts

--- custom.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-declare module "*.svg" {
-  import type * as React from "react";
-
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export { ReactComponent };
-}
